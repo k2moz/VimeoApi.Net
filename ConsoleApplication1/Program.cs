15f787a61fe66842c28371b2bfff720b0d34be02
a.sazhin <a.sazhin@itway.company>
2019-04-01 18:44:50 +0300

    d

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -12,164 +12,203 @@ using System.Threading.Tasks;
 
 namespace ConsoleApplication1
 {
-	class Program
-	{
-		static void Main( string[] args )
-		{
-			Program p = new Program();
-			//p.POSTUploadFileFromPath( "C://SASFiles/video.mp4" );
-			
-			p.PullUploadFileFromPath( "C://SASFiles/video.mp4" );
-			Console.ReadKey();
-		}
+    class Program
+    {
+        public const string accessToken = "";
+        public const string cid = "";
+        public const string secret = "";
 
-		/// <summary>
-		/// 1. Simple HTTP POST uploading
-		/// </summary>
-		/// <param name="path">Local path from file</param>
-		public void POSTUploadFileFromPath( string path )
-		{
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            //p.POSTUploadFileFromPath( "C://SASFiles/video.mp4" );
 
-			var vc = Vimeo.VimeoClient.ReAuthorize(
-			accessToken: "accessToken",
-			cid: "cid",
-			secret: "secret"
-			 );
+            var video = p.PullUploadFileFromPath("C://SAS/video.mp4");
+            p.SetPrivateStatus(video.VideoId.ToString(), "brainoteka.com");
+            Console.ReadKey();
+        }
 
-			Dictionary<string, string> dic = new Dictionary<string, string>();
-			dic.Add( "type", "POST" );
-			dic.Add( "redirect_url", "http://google.com/" );
+        /// <summary>
+        /// 1. Simple HTTP POST uploading
+        /// </summary>
+        /// <param name="path">Local path from file</param>
+        public void POSTUploadFileFromPath(string path)
+        {
 
-			var my2 = vc.Request( "/me/videos", dic, "POST" );
-			var linkSequere = my2["uri"].ToString();
+            var vc = Vimeo.VimeoClient.ReAuthorize(
+            accessToken: accessToken,
+            cid: cid,
+            secret: secret
+             );
 
-			WebClient myWebClient = new WebClient();
+            Dictionary<string, string> dic = new Dictionary<string, string>();
+            dic.Add("type", "POST");
+            dic.Add("redirect_url", "http://google.com/");
 
-			string fileName = path;
-			byte[] responseArray = myWebClient.UploadFile( my2["upload_link_secure"].ToString(), "POST", fileName );
+            var my2 = vc.Request("/me/videos", dic, "POST");
+            var linkSequere = my2["uri"].ToString();
 
-			var finaddr = myWebClient.BaseAddress;
-			/*Console.WriteLine(System.Text.Encoding.ASCII.GetString( responseArray ) );*/
-		}
+            WebClient myWebClient = new WebClient();
 
-		/// <summary>
-		/// Resumable (Но это не точно) HTTP PUT uploads
-		/// https://developer.vimeo.com/api/upload/videos
-		/// </summary>
-		/// <param name="videoFilePath">Local path to file</param>
-		/// <param name="authToken">user oAuth token</param>
-		/// <param name="tikket">Upload accessible token</param>
-		/// <param name="uploadLinkSecure">secure link for upload action</param>
-		/// <param name="completeUrl">link finished upload transaction</param>
-		/// <returns>Exeption text or result video link </returns>
-		public VimeoVideoWrapper PullUploadFileFromPath( string path )
-		{
-			
-			var vc = Vimeo.VimeoClient.ReAuthorize(
-						accessToken: "accessToken",
-						cid: "cid",
-						secret: "secret"
-						 );
-			Dictionary<string, string> dic = new Dictionary<string, string>();
-			dic.Add( "type", "streaming" );
+            string fileName = path;
+            byte[] responseArray = myWebClient.UploadFile(my2["upload_link_secure"].ToString(), "POST", fileName);
 
-			#region Test
-			var test_var=GetThumb( "221619251", vc.AccessToken );
-			#endregion
-			VimeoVideoWrapper video = new VimeoVideoWrapper();
-			var my2 = vc.Request( "/me/videos", dic, "POST" );
-			var tikket = my2["uri"].ToString();
+            var finaddr = myWebClient.BaseAddress;
+            /*Console.WriteLine(System.Text.Encoding.ASCII.GetString( responseArray ) );*/
+        }
 
-			try
-			{
-				Uri uri = new Uri( my2["upload_link_secure"].ToString() );
-				
-				video.VideoId = long.Parse(uri.Query.Split( '&' ).FirstOrDefault( x => x.StartsWith( "video_file_id=" ) ).Replace( "video_file_id=", "" )) ;
-				video.VideoLink= SendFileToServer( path, vc.AccessToken, tikket, my2["upload_link_secure"].ToString(), my2["complete_uri"].ToString() );
-				video.Thumb = GetThumb( video.VideoLink.Split( '/' ).Last(), vc.AccessToken ); 
-			}
-			catch(Exception e)
-			{
-				video.Error = true;
-				video.ErrorText = e.Message;
-			}
-			return video;
-		}
+        /// <summary>
+        /// Resumable (Но это не точно) HTTP PUT uploads
+        /// https://developer.vimeo.com/api/upload/videos
+        /// </summary>
+        /// <param name="videoFilePath">Local path to file</param>
+        /// <param name="authToken">user oAuth token</param>
+        /// <param name="tikket">Upload accessible token</param>
+        /// <param name="uploadLinkSecure">secure link for upload action</param>
+        /// <param name="completeUrl">link finished upload transaction</param>
+        /// <returns>Exeption text or result video link </returns>
+        public VimeoVideoWrapper PullUploadFileFromPath(string path)
+        {
 
-		/// <summary>
-		/// Тут ещё нужно будет дописать логику на проверку "загрузился ли файл полностью" и вообще огонь будет
-		/// </summary>
-		/// <param name="videoFilePath"></param>
-		/// <param name="authToken"></param>
-		/// <param name="tikket"></param>
-		/// <param name="uploadLinkSecure"></param>
-		/// <param name="completeUrl"></param>
-		/// <returns></returns>
-		private string SendFileToServer( string videoFilePath, string authToken, string tikket, string uploadLinkSecure, string completeUrl )
-		{
+            var vc = Vimeo.VimeoClient.ReAuthorize(
+                        accessToken: accessToken,
+                        cid: cid,
+                        secret: secret
+                         );
+            Dictionary<string, string> dic = new Dictionary<string, string>();
+            dic.Add("type", "streaming");
 
-			StringBuilder sendData = new StringBuilder();
+            #region Test
+            var test_var = GetThumb("221619251", vc.AccessToken);
+            #endregion
+            VimeoVideoWrapper video = new VimeoVideoWrapper();
+            var my2 = vc.Request("/me/videos", dic, "POST");
+            var tikket = my2["uri"].ToString();
 
-			using ( FileStream img = new FileStream( videoFilePath, FileMode.Open ) )
-			{
-				sendData.Append( "file_data=" + img.ToString() );
-			}
-			byte[] jsonBytes = Encoding.GetEncoding( 1251 ).GetBytes( sendData.ToString() );
+            try
+            {
+                Uri uri = new Uri(my2["upload_link_secure"].ToString());
 
-			using ( var fileStream = new FileStream( videoFilePath, FileMode.Open ) )
-			{
+                video.VideoId = long.Parse(uri.Query.Split('&').FirstOrDefault(x => x.StartsWith("video_file_id=")).Replace("video_file_id=", ""));
+                video.VideoLink = SendFileToServer(path, vc.AccessToken, tikket, my2["upload_link_secure"].ToString(), my2["complete_uri"].ToString());
+                video.Thumb = GetThumb(video.VideoLink.Split('/').Last(), vc.AccessToken);
+            }
+            catch (Exception e)
+            {
+                video.Error = true;
+                video.ErrorText = e.Message;
+            }
+            return video;
+        }
 
-				HttpWebRequest request = ( HttpWebRequest )WebRequest.Create( uploadLinkSecure );
+        /// <summary>
+        /// Тут ещё нужно будет дописать логику на проверку "загрузился ли файл полностью" и вообще огонь будет
+        /// </summary>
+        /// <param name="videoFilePath"></param>
+        /// <param name="authToken"></param>
+        /// <param name="tikket"></param>
+        /// <param name="uploadLinkSecure"></param>
+        /// <param name="completeUrl"></param>
+        /// <returns></returns>
+        private string SendFileToServer(string videoFilePath, string authToken, string tikket, string uploadLinkSecure, string completeUrl)
+        {
 
-				request.Method = "PUT";
-				request.Headers.Add( HttpRequestHeader.Authorization, "Bearer " + authToken );
-				request.ContentLength = fileStream.Length;
-				request.ContentType = "video/mp4";
-				using ( Stream dataStream = request.GetRequestStream() )
-				{
-					byte[] buffer = new byte[fileStream.Length];
-					var data = fileStream.Read( buffer, 0, buffer.Length );
-					dataStream.Write( buffer, 0, data );
-				}
-				try
-				{
-					using ( HttpWebResponse response = ( HttpWebResponse )request.GetResponse() )
-					{
-						request = ( HttpWebRequest )WebRequest.Create( "https://api.vimeo.com"+completeUrl );
+            StringBuilder sendData = new StringBuilder();
 
-						request.Method = "DELETE";
-						request.Headers.Add( HttpRequestHeader.Authorization, "Bearer " + authToken );
+            using (FileStream img = new FileStream(videoFilePath, FileMode.Open))
+            {
+                sendData.Append("file_data=" + img.ToString());
+            }
+            byte[] jsonBytes = Encoding.GetEncoding(1251).GetBytes(sendData.ToString());
 
-						using ( HttpWebResponse finishResponse = ( HttpWebResponse )request.GetResponse() )
-						{
-							return finishResponse.Headers["Location"];
-						}
+            using (var fileStream = new FileStream(videoFilePath, FileMode.Open))
+            {
 
-					}
-				}
-				catch ( WebException ex )
-				{
-					return ex.ToString() ;
-				}
-			}
-		}
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uploadLinkSecure);
 
-		private string GetThumb(string videoId,string authToken)
-		{
-			string result = "";
-			HttpWebRequest request = ( HttpWebRequest )WebRequest.Create( "https://vimeo.com/api/oembed.json?url=https%3A//vimeo.com/"+videoId );
+                request.Method = "PUT";
+                request.Headers.Add(HttpRequestHeader.Authorization, "Bearer " + authToken);
+                request.ContentLength = fileStream.Length;
+                request.ContentType = "video/mp4";
+                using (Stream dataStream = request.GetRequestStream())
+                {
+                    byte[] buffer = new byte[fileStream.Length];
+                    var data = fileStream.Read(buffer, 0, buffer.Length);
+                    dataStream.Write(buffer, 0, data);
+                }
+                try
+                {
+                    using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+                    {
+                        request = (HttpWebRequest)WebRequest.Create("https://api.vimeo.com" + completeUrl);
 
-			request.Method = "GET";
-			request.Headers.Add( HttpRequestHeader.Authorization, "Bearer " + authToken );
+                        request.Method = "DELETE";
+                        request.Headers.Add(HttpRequestHeader.Authorization, "Bearer " + authToken);
 
-			using ( HttpWebResponse response = ( HttpWebResponse )request.GetResponse() )
-			{
-				var strreader = new StreamReader( response.GetResponseStream(), Encoding.UTF8 );
-				var responseToString = strreader.ReadToEnd();
-				var a = JsonConvert.DeserializeObject<JObject> (responseToString);
-				return a["thumbnail_url"].ToString();
-			}
-			return result;
-		}
-	}
+                        using (HttpWebResponse finishResponse = (HttpWebResponse)request.GetResponse())
+                        {
+                            return finishResponse.Headers["Location"];
+                        }
+
+                    }
+                }
+                catch (WebException ex)
+                {
+                    return ex.ToString();
+                }
+            }
+        }
+
+        private string GetThumb(string videoId, string authToken)
+        {
+            string result = "";
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("https://vimeo.com/api/oembed.json?url=https%3A//vimeo.com/" + videoId);
+
+            request.Method = "GET";
+            request.Headers.Add(HttpRequestHeader.Authorization, "Bearer " + authToken);
+
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            {
+                var strreader = new StreamReader(response.GetResponseStream(), Encoding.UTF8);
+                var responseToString = strreader.ReadToEnd();
+                var a = JsonConvert.DeserializeObject<JObject>(responseToString);
+                return a["thumbnail_url"].ToString();
+            }
+            return result;
+        }
+
+        private string SetPrivateStatus(string videoId, string domain, string authToken = "")
+        {
+            //Permit a video to be embedded on a domain
+            //privacy.embed
+            //whitelist
+            //PUThttps://api.vimeo.com/videos/{video_id}/privacy/domains/{domain}
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("https://vimeo.com/api/videos/" + videoId + "/privacy/domains/" + domain);
+            request.Method = "GET";
+            if (!string.IsNullOrEmpty(authToken))
+            {
+                var vc = Vimeo.VimeoClient.ReAuthorize(
+                        accessToken: accessToken,
+                        cid: cid,
+                        secret: secret
+                         );
+                request.Headers.Add(HttpRequestHeader.Authorization, "Bearer " + vc.AccessToken);
+            }
+            else
+            {
+                request.Headers.Add(HttpRequestHeader.Authorization, "Bearer " + authToken);
+            }
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            {
+                var strreader = new StreamReader(response.GetResponseStream(), Encoding.UTF8);
+                var responseToString = strreader.ReadToEnd();
+                var a = JsonConvert.DeserializeObject<JObject>(responseToString);
+                //return a["thumbnail_url"].ToString();
+            }
+
+
+            return null;
+        }
+    }
 }
